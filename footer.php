e744352ca83997b2cbed9d9a946a45a145e53d36
Frank Barrera - @farkbarn <frankbarreraag@gmail.com>
2026-05-14 16:53:01 -0500

    sin cambions de momento

---
+++ footer.php
@@ -0,0 +1,26 @@
+<section class="footer">
+  <section class="SecPie">
+    <section class="SecLog">
+      <img class="ico" src="img/ico.png" alt="">
+    </section>
+    <section class="SecRRSS">
+      <img class="ImgLin" src="img/Linkedin.png" alt="">
+    </section>
+    <section class="SecCon">
+      <p class="PCon">contact us</p>
+      <p class="P311">3116 Cortland </br>Chicago, IL 60647</p>
+      <p class="P630">630.870.2141 </br>letsfly@web.com</p>
+    </section>
+    <section class="SecLin">
+      <a class="SecLinAOur" href="#">our services</a>
+      <a class="SecLinBel" href="#">beliefs</a>
+      <a class="SecLinAboUs" href="#">about us</a>
+      <a class="SecLinJoiUs" href="#">join us</a>
+    </section>
+  </section>
+</section>
+
+</section>
+</main>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>TakeOff</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Sitio Web TakeOff" />
+    <meta name="robots" content="index,follow" />
+    <meta name="keywords" content="take,off,takeoff"/>
+    <meta name="author" content="Frank Barrera | @farkbarn"/>
+    <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url'); ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri()."/img/ico.png"; ?>">
+  </head>
+  <body class="body">
+    <main class="main">
+      <section class="principal">
+        <section class="icono">
+          <img class="ico" src="<?php echo get_template_directory_uri()."/img/ico.png"; ?>" alt="">
+        </section>
+        <ul class="menu">
+          <li> <a href="<?php echo get_site_url()."/?cat=1"; ?>">our services</a> </li>
+          <li> <a href="<?php echo get_site_url()."/?cat=2"; ?>">beliefs</a> </li>
+          <li> <a href="<?php echo get_site_url()."/?cat=3"; ?>">case studies</a> </li>
+          <li> <a href="<?php echo get_site_url()."/?cat=4"; ?>">about us</a> </li>
+          <li> <a href="<?php echo get_site_url()."/?cat=5"; ?>">join us</a> </li>
+        </ul>
